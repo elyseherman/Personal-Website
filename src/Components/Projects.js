7b7f84676dcf16b874e5d29b8e9b895c137265c1
Elyse Herman <elyse_herman@hotmail.com>
2024-12-16 22:13:52 -0500

    Updated Projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,6 +4,7 @@ import ProjectsCard from './ProjectsCard';
 import ProjectsInProgressCard from './ProjectsInProgress';
 import image1 from '../Media/elyseherman.com.png';
 import image2 from '../Media/ZombieLand.png';
+import image3 from '../Media/McBooking.png';
 
 class Projects extends Component {
     render() {
@@ -11,7 +12,7 @@ class Projects extends Component {
             <div className='projects'>
                 <ProjectsInProgressCard projectTitle='WeekBite · Grocery Helper' description='An app that makes grocery shopping seamless and stress-free. Upload your favourite recipes, organzie your weekly meal plan, and generate your grocery list with all necessary ingredients and quantities.' tag1='React Native' tag2='JavaScript' tag3='HTML' tag4='CSS' />
                 <a href="https://github.com/SimonjArts/mcbooking" target='_blank' rel='noopener noreferrer'>
-                    <ProjectsCard projectTitle='McBooking' description='An appointment-booking platform designed for professors, TAs, and students at McGill University. The system streamlines the process of scheduling office hours, managing bookings, and improving communication between students and academic staff. Built with a focus on usability and efficiency, MCBooking features user authentication, session management, and a clean, intuitive interface to simplify academic scheduling.' tag1='PHP' tag2='HTML' tag3='CSS' tag4='JavaScript' />
+                    <ProjectsCard projectTitle='McBooking' description='An appointment-booking platform designed for professors, TAs, and students at McGill University. The system streamlines the process of scheduling office hours, managing bookings, and improving communication between students and academic staff. Built with a focus on usability and efficiency, MCBooking features user authentication, session management, and a clean, intuitive interface to simplify academic scheduling.' tag1='PHP' tag2='HTML' tag3='CSS' tag4='JavaScript' src={image3} />
                 </a>
                 <ProjectsCard projectTitle='Personal Website' description='A digital portfolio that showcases my projects and experience as a software developer, as well as my personal highlights.' tag1='React' tag2='JavaScript' tag3='HTML' tag4='CSS' src={image1} />
                 <a href='https://github.com/elyseherman/ZombieLand' target='_blank' rel='noopener noreferrer'>
